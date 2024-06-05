31692c324886a8c57e45de8b195a4332ae15c126
Danh Tran <danh2k@nexondv.com>
2024-06-05 14:58:19 +0700

    redis-gotcha-playground

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,15 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+	await client.hSet('car2', {
+		color: 'red',
+		year: 1995
+		// isUsed: true
+	});
+
+	const car = await client.hGetAll('car2');
+	console.log(car);
+};
+
 run();
